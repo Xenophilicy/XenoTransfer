de8e21ced1d1ffacf18bd2f714561d521cb8ed20
Xenophilicy <Xenophilicy@users.noreply.github.com>
2018-12-28 03:20:46 -0600

    Update v2.7.3

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 # [![Xenophilicy](https://i.imgur.com/4Hcu5Ms.png)]()
 
 ```diff
-+ Updated Version To 2.7.0
-+ Fixed compass issue
-+ Added versatility to config file
-- Removed useless scheduler
++ Updated Version To 2.7.3
++ Added support for removing compass on player quit
++ Added support to customize item type, ex: 342=Compass, 1=Stone etc.
++ Condensed code for practicality
+- Removed excess calling of 'serverList function'
 ```
+
+***IMPORTANT:*** *This is a title update to the plugin, your current config file will **NOT** work on this version! Please update your config.yml!*
+
 ## Information
-Finally, A plugin that allows players to transfer between servers with simplicity.
-**XenoTransfer** is a plugin that simply uses **FormAPI** to easly and seamlessly display your servers in a ordered list. Its simple, the user just needs to write a simple command, and then from there its pretty straightforward.
+Finally, A plugin that allows players to transfer between servers with simplicity. **XenoTransfer** is a plugin that simply uses **FormAPI** to easly and seamlessly display your servers in a ordered list. Its simple, the user just needs to enter their server IPs and Ports in the config file, and after that, you're good to go!
 
 ## Commands
 ```diff
@@ -19,8 +22,8 @@ Finally, A plugin that allows players to transfer between servers with simplicit
 ***
 
 ## XenoTansfer Details
-* **API:** 3.5.1
-* **Version:** 2.7.0
+* **API:** 3.5.0+
+* **Version:** 2.7.3
 * **Basic Description:** Lets you transfer between your server network with forms!
 * *Easy to edit config.yml file*
 * *Simple code for editing and debugging*
--- src/XenoTransfer/main.php
+++ src/XenoTransfer/main.php
@@ -34,6 +34,8 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\enchantment\EnchantmentInstance;
@@ -41,6 +43,8 @@ use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\utils\Config;
 use pocketmine\item\Item;
+use pocketmine\inventory\BaseTransaction;
+use pocketmine\inventory\Inventory;
 
 //FormAPI → https://github.com/jojoe77777/FormAPI
 use jojoe77777\FormAPI;
@@ -60,8 +64,16 @@ class Main extends PluginBase implements Listener {
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info("§6XenoTransfer§a has been enabled!");
+        $selectorEnable = $this->config->get("Selector-Support");
+        if ($selectorEnable == true) {
+            $this->selectorSupport = true;
+        }
+        else {
+            $this->selectorSupport = false;
+            $this->getLogger()->notice("§eSelector item support turned off in config! Disabling selector...");
+        }
     }
-    
+
     public function onDisable(){
         $this->getLogger()->info($this->config->get("§6XenoTransfer§c has been disabled!"));
     }
@@ -83,7 +95,7 @@ class Main extends PluginBase implements Listener {
             return true;
         }
         else {
-            $sender->sendMessage("§cThis command only works in game.");
+            $sender->sendMessage("§cThis is an in-game command only!");
             return true;
         }
     }
@@ -137,74 +149,41 @@ class Main extends PluginBase implements Listener {
     }
 
     public function onJoin(PlayerJoinEvent $event){
-        $compEnable = $this->config->get("Compass-Hotbar");
-        if ($compEnable == true) {
+        if ($this->selectorSupport == true) {
             $player = $event->getPlayer();
-            $compassText = $this->config->get("Compass-Name");
+            $selectorText = $this->config->get("Selector-Name");
             $enchantment = Enchantment::getEnchantment(0);
             $enchInstance = new EnchantmentInstance($enchantment, 1);
-            $item = Item::get(345);
-            $item->setCustomName("§o$compassText");
+            $itemType = $this->config->get("Selector-Item");
+            $item = Item::get($itemType);
+            $item->setCustomName("§o$selectorText");
             $item->addEnchantment($enchInstance);
             $player->getInventory()->addItem($item);
         }
         else {
-            $this->getLogger()->notice("§eCompass support turned off in conig! Disabling compass...");
+        }
+    }
+
+    public function onQuit(PlayerQuitEvent $event){
+        $player = $event->getPlayer();
+        $items = $player->getInventory()->getContents();
+        $selectorText = $this->config->get("Selector-Name");
+        foreach($items as $target) {
+            if($target->getCustomName() == "§o$selectorText") {
+                $player->getInventory()->remove($target);
+            }
+            return false;
         }
     }
 
     public function onInteract(PlayerInteractEvent $event){
-        $compEnable = $this->config->get("Compass-Hotbar");
-        if ($compEnable == true) {
+        if ($this->selectorSupport == true) {
             $player = $event->getPlayer();
-            $compassText = $this->config->get("Compass-Name");
+            $selectorText = $this->config->get("Selector-Name");
+            $itemType = $this->config->get("Selector-Item");
             $item = $player->getInventory()->getItemInHand();
-            if($item->getCustomName() == "§o$compassText"){
-                $formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-                $form = new SimpleForm(function (Player $player, $data){
-                    if($data === null){
-                        return;
-                    }
-                    switch($data){
-                        case 0:
-                            $this->getServer()->getCommandMap()->dispatch($player, 'transferserver '.$this->config->getNested("Server-1.IP").' '.$this->config->getNested("Server-1.Port"));
-                            break;
-                        case 1:
-                            $this->getServer()->getCommandMap()->dispatch($player, 'transferserver '.$this->config->getNested("Server-2.IP").' '.$this->config->getNested("Server-2.Port"));
-                            break;
-                        case 2:
-                            $this->getServer()->getCommandMap()->dispatch($player, 'transferserver '.$this->config->getNested("Server-3.IP").' '.$this->config->getNested("Server-3.Port"));
-                            break;
-                        case 3:
-                            $this->getServer()->getCommandMap()->dispatch($player, 'transferserver '.$this->config->getNested("Server-4.IP").' '.$this->config->getNested("Server-4.Port"));
-                            break;
-                        case 4:
-                            $this->getServer()->getCommandMap()->dispatch($player, 'transferserver '.$this->config->getNested("Server-5.IP").' '.$this->config->getNested("Server-5.Port"));
-                            break;
-                    }
-                    return true;
-            });
-            $form->setTitle($this->config->getNested("UI-Title"));
-            $form->setContent($this->config->getNested("UI-Message"));
-            $label1 = $this->config->getNested("Server-1.Label");
-            $label2 = $this->config->getNested("Server-2.Label");
-            $label3 = $this->config->getNested("Server-3.Label");
-            $label4 = $this->config->getNested("Server-4.Label");
-            $label5 = $this->config->getNested("Server-5.Label");
-            $form->addButton("$label1");
-            if ($label2 != false){
-                $form->addButton("$label2");
-            }
-            if ($label3 != false){
-                $form->addButton("$label3");
-            }
-            if ($label4 != false){
-                $form->addButton("$label4");
-            }
-            if ($label5 != false){
-                $form->addButton("$label5");
-            }
-            $form->sendToPlayer($player);
+            if($item->getCustomName() == "§o$selectorText" && $item->getId() == $itemType){
+                $this->serverList($player);
             }
         }
         else{
